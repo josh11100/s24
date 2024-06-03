8d1abe3c3f0f9c2537866a7e5652495196f4b534
joshua chung <joshuachung@ucsb.edu>
2024-06-03 03:12:36 -0700

    test

--- labs/voxhop/main.cpp
+++ labs/voxhop/main.cpp
@@ -5,13 +5,13 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-  if(argc != 2) {
+  if (argc != 2) {
     std::cerr << "USAGE: " << argv[0] << " [map-file]\n";
     return 1;
   }
 
   std::ifstream stream(argv[1]);
-  if(stream.fail()) {
+  if (stream.fail()) {
     std::cerr << "ERROR: Could not open file: " << argv[1] << '\n';
     return 1;
   }
@@ -21,15 +21,20 @@ int main(int argc, char** argv) {
 
   Point src;
   Point dst;
-  while(std::cin >> src >> dst) {
+  while (std::cin >> src >> dst) {
     try {
+      // Check if the source and destination points are valid before finding a route
+      if (!map.isValidVoxel(src.x, src.y, src.z)) {
+        throw InvalidPoint(src);
+      }
+      if (!map.isValidVoxel(dst.x, dst.y, dst.z)) {
+        throw InvalidPoint(dst);
+      }
       Route route = map.route(src, dst);
       std::cout << route << '\n';
-    }
-    catch(const InvalidPoint& err) {
+    } catch (const InvalidPoint& err) {
       std::cout << "Invalid point: " << err.point() << '\n';
-    }
-    catch(const NoRoute& err) {
+    } catch (const NoRoute& err) {
       std::cout << "No route from " << err.src() << " to " << err.dst() << ".\n";
     }
   }
